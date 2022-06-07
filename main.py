92fdb9f1403350ad6e11b16ebe364f93e6e143aa
SHIVAMTYAGI2309 <64794844+SHIVAMTYAGI2309@users.noreply.github.com>
2022-06-08 00:52:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+with open('C:/Users/shalu/Desktop/example.txt', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('placement', 'screening')
+
+# Write the file out again
+with open('C:/Users/shalu/Desktop/example.txt', 'w') as file:
+  file.write(filedata)
